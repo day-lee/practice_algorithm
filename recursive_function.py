beaa69117763e586afa9aa06f4c157032fae17af
dayeon <iamdayeonlee@gmail.com>
2021-05-31 18:19:26 +0900

    Add flipped list

--- recursive_function.py
+++ recursive_function.py
@@ -43,7 +43,7 @@ for i in range(1, 11):
 # 55
 
 #3 Sum of Digits
-
+# retuns sum of each digits, which is parameter 'n'
 def sum_digits(n):
     if n < 10:
         return n
@@ -73,3 +73,17 @@ def sum_digits(n):
     return n % 10 + sum_digits(n // 10)
 
 print(sum_digits(22541))
+
+#4 Flipped list
+# returns flipped list of parameter some_list
+def flip(some_list):
+    if len(some_list) <= 1:
+        return some_list
+
+    return [some_list[-1]] + flip(some_list[0:-1])
+
+
+# test
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = flip(some_list)
+print(some_list)
